fda2d8f6b5c71262e33763f8287d28fbb7a127fc
jayman99 <jayman.mu@gmail.com>
2019-12-02 19:15:28 +0800

    mmm

--- src/views/LaunchBid.vue
+++ src/views/LaunchBid.vue
@@ -2,92 +2,111 @@
   <div>
     <el-form
       :inline="true"
+      :rules="rules"
       :label-position="labelPosition"
       label-width="150px"
-      :model="formLabelAlign"
+      :model="ruleForm"
+      ref="ruleForm"
     >
-      <el-form-item label="您的称呼">
-        <el-input v-model="formLabelAlign.linkMan"></el-input>
+      <el-form-item label="联系人" prop="linkMan">
+        <el-input v-model="ruleForm.linkMan"></el-input>
       </el-form-item>
-      <el-form-item label="手机号码">
-        <el-input v-model="formLabelAlign.phone"></el-input>
+      <el-form-item label="联系方式" prop="phone">
+        <el-input v-model="ruleForm.phone"></el-input>
       </el-form-item>
-      <el-form-item label="详细地址">
-        <el-input v-model="formLabelAlign.Telephone"></el-input>
+      <el-form-item label="项目名称" prop="calusename">
+        <el-input v-model="ruleForm.calusename"></el-input>
       </el-form-item>
-      <el-form-item label="装修面积">
-        <el-input v-model="formLabelAlign.area"></el-input>
+      <el-form-item label="装修面积" prop="area">
+        <el-input v-model="ruleForm.area"></el-input>
       </el-form-item>
-      <el-form-item label="动工时间">
-        <el-form-item prop="date1">
-          <el-date-picker type="date" placeholder="选择日期" v-model="formLabelAlign.startTime" style="width: 100%;"></el-date-picker>
+      <el-form-item label="地点" prop="location">
+        <el-input v-model="ruleForm.location"></el-input>
+      </el-form-item>
+      <el-form-item label="工期" prop="timeForProject">
+        <el-input v-model="ruleForm.timeForProject"></el-input>
+      </el-form-item>
+      <el-form-item label="预计开工时间">
+        <el-form-item prop="startTime">
+          <el-date-picker
+            type="date"
+            placeholder="选择日期"
+            v-model="ruleForm.startTime"
+            style="width: 190px;"
+          ></el-date-picker>
         </el-form-item>
       </el-form-item>
+      <el-form-item label="户型结构" prop="familyStructure">
+        <el-select v-model="ruleForm.familyStructure" placeholder="未选择" style="width: 202px;">
+          <el-option label="一居室" value="oneBedroom"></el-option>
+          <el-option label="两居室" value="twoBedrooms"></el-option>
+          <el-option label="三居室" value="threeBedrooms"></el-option>
+          <el-option label="四居室" value="fourBedrooms"></el-option>
+          <el-option label="复式" value="compound"></el-option>
+          <el-option label="别墅" value="villa"></el-option>
+          <el-option label="跃层" value="leapLayer"></el-option>
+          <el-option label="其他结构" value="otherStructures"></el-option>
+        </el-select>
+      </el-form-item>
+
       <el-form-item label="招标开始时间">
-        <el-form-item prop="date1">
-          <el-date-picker type="date" placeholder="选择日期" v-model="formLabelAlign.tenderStartTime" style="width: 100%;"></el-date-picker>
+        <el-form-item prop="tenderStartTime">
+          <el-date-picker
+            type="date"
+            placeholder="选择日期"
+            v-model="ruleForm.tenderStartTime"
+            style="width: 190px;"
+          ></el-date-picker>
         </el-form-item>
       </el-form-item>
-      
-      <!-- <el-form-item label="装修空间">
-        <el-select v-model="formLabelAlign.realName" placeholder="未选择">
-          <el-option label="住宅公寓" value=""></el-option>
-          <el-option label="别墅" value=""></el-option>
-          <el-option label="办公空间" value=""></el-option>
-          <el-option label="厂房" value=""></el-option>
-          <el-option label="展示厅" value=""></el-option>
-          <el-option label="展会空间" value=""></el-option>
-          <el-option label="商场超市" value=""></el-option>
-          <el-option label="专卖店" value=""></el-option>
-          <el-option label="休闲健身" value=""></el-option>
-          <el-option label="歌舞厅" value=""></el-option>
-          <el-option label="酒店宾馆" value=""></el-option>
-          <el-option label="餐饮酒吧" value=""></el-option>
-          <el-option label="茶楼" value=""></el-option>
-          <el-option label="会所" value=""></el-option>
-          <el-option label="学校" value=""></el-option>
-          <el-option label="网吧" value=""></el-option>
-          <el-option label="其他" value=""></el-option>
-        </el-select>
-      </el-form-item> -->
-      <el-form-item label="户型结构">
-        <el-select v-model="formLabelAlign.familyStructure" placeholder="未选择">
-          <el-option label="一居室" value=""></el-option>
-          <el-option label="两居室" value=""></el-option>
-          <el-option label="三居室" value=""></el-option>
-          <el-option label="四居室" value=""></el-option>
-          <el-option label="复式" value=""></el-option>
-          <el-option label="别墅" value=""></el-option>
-          <el-option label="跃层" value=""></el-option>
-          <el-option label="其他结构" value=""></el-option>
+
+      <el-form-item label="房屋现状" prop="housingSituation">
+        <el-select v-model="ruleForm.housingSituation" placeholder="未选择" style="width: 202px;">
+          <el-option label="未买房" value="weimai"></el-option>
+          <el-option label="未交房" value="weijiao"></el-option>
+          <el-option label="正准备装修" value="zhunbei"></el-option>
+          <el-option label="已定好装修公司" value="dinghao"></el-option>
+          <el-option label="已开始装修" value="kaishi"></el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="房屋现状">
-        <el-select v-model="formLabelAlign.housingSituation" placeholder="未选择">
-          <el-option label="未买房" value=""></el-option>
-          <el-option label="未交房" value=""></el-option>
-          <el-option label="正准备装修" value=""></el-option>
-          <el-option label="已定好装修公司" value=""></el-option>
-          <el-option label="已开始装修" value=""></el-option>
+
+      <el-form-item label="招标结束时间">
+        <el-form-item prop="tenderEndTime">
+          <el-date-picker
+            type="date"
+            placeholder="选择日期"
+            v-model="ruleForm.tenderEndTime"
+            style="width: 190px;"
+          ></el-date-picker>
+        </el-form-item>
+      </el-form-item>
+
+      <el-form-item label="装修预算" prop="decorationBudget">
+        <el-select v-model="ruleForm.decorationBudget" placeholder="未选择" style="width: 202px;">
+          <el-option label="3万以内" value="3"></el-option>
+          <el-option label="3至5万" value="3-5"></el-option>
+          <el-option label="5至8万" value="5-8"></el-option>
+          <el-option label="8至10万" value="8-10"></el-option>
+          <el-option label="10至15万" value="10-15"></el-option>
+          <el-option label="15至20万" value="15-20"></el-option>
+          <el-option label="20至30万" value="20-30"></el-option>
+          <el-option label="30至50万" value="30-50"></el-option>
+          <el-option label="50至100万" value="50-100"></el-option>
+          <el-option label="100至200万" value="100-200"></el-option>
+          <el-option label="200万以上" value="200+"></el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="装修预算">
-        <el-select v-model="formLabelAlign.decorationBudget" placeholder="未选择">
-          <el-option label="3万以内" value=""></el-option>
-          <el-option label="3至5万" value=""></el-option>
-          <el-option label="5至8万" value=""></el-option>
-          <el-option label="8至10万" value=""></el-option>
-          <el-option label="10至15万" value=""></el-option>
-          <el-option label="15至20万" value=""></el-option>
-          <el-option label="20至30万" value=""></el-option>
-          <el-option label="30至50万" value=""></el-option>
-          <el-option label="50至100万" value=""></el-option>
-          <el-option label="100至200万" value=""></el-option>
-          <el-option label="200万以上" value=""></el-option>
-        </el-select>
+    </el-form>
+    <el-form :model="ruleForm" :rules="rules" ref="ruleForm">
+      <el-form-item label="装修详细要求" style="margin-left: 54px;" prop="fitmentRequest">
+        <el-input type="textarea" rows="4" v-model="ruleForm.fitmentRequest" style="width: 560px;"></el-input>
       </el-form-item>
-      <el-form-item label="装修要求">
-        <el-input type="textarea" v-model="formLabelAlign.fitmentRequest"></el-input>
+      <el-form-item label="投标公司要求" style="margin-left: 54px;" prop="compayRequest">
+        <el-input type="textarea" rows="4" v-model="ruleForm.compayRequest" style="width: 560px;"></el-input>
+      </el-form-item>
+      <el-form-item style="margin-left: 54px;">
+        <el-button type="primary" @click="submitForm('ruleForm')">立即发布</el-button>
+        <el-button @click="resetForm('ruleForm')">重置</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -98,22 +117,147 @@ export default {
   data() {
     return {
       labelPosition: "right",
-      formLabelAlign: {
-        userName: '',
-        realName: '',
-        userPhone: '',
-        userSex: '',
-        userBirthday: '',
-        Telephone: '',
-        userQQ: '',
-        userEmail: ''
+      ruleForm: {
+        linkMan: "",
+        phone: "",
+        calusename: "",
+        area: "",
+        location: "",
+        timeForProject: "",
+        startTime: "",
+        familyStructure: "",
+        tenderStartTime: "",
+        housingSituation: "",
+        tenderEndTime: "",
+        decorationBudget: "",
+        fitmentRequest: "",
+        compayRequest: ""
+      },
+      rules: {
+        linkMan: [
+          { required: true, message: "请输入联系人名称", trigger: "blur" },
+          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" }
+        ],
+        phone: [
+          { required: true, message: "请输入联系方式", trigger: "blur" },
+          {
+            pattern: /^[1][3,4,5,7,8][0-9]{9}$/,
+            message: "手机号码格式不正确",
+            trigger: "blur"
+          }
+        ],
+        calusename: [
+          { required: true, message: "请输入项目名称", trigger: "blur" }
+        ],
+        area: [
+          { required: true, message: "请填写装修面积", trigger: "blur" },
+          {
+            pattern: /[1-9]\d*/,
+            message: "请输入一个正整数（平方米）",
+            trigger: "blur"
+          }
+        ],
+        location: [{ required: true, message: "请填写地点", trigger: "blur" }],
+        timeForProject: [
+          { required: true, message: "请填写工期", trigger: "blur" }
+        ],
+        startTime: [
+          {
+            type: "date",
+            required: true,
+            message: "请填写开工时间",
+            trigger: "change"
+          }
+        ],
+        familyStructure: [
+          { required: true, message: "请选择户型结构", trigger: "change" }
+        ],
+        tenderStartTime: [
+          {
+            type: "date",
+            required: true,
+            message: "请填招标开始时间",
+            trigger: "change"
+          }
+        ],
+        housingSituation: [
+          { required: true, message: "请选择房屋现状", trigger: "change" }
+        ],
+        tenderEndTime: [
+          {
+            type: "date",
+            required: true,
+            message: "请填写招标结束时间",
+            trigger: "change"
+          }
+        ],
+        decorationBudget: [
+          { required: true, message: "请选择装修预算费用", trigger: "change" }
+        ],
+        fitmentRequest: [
+          { required: true, message: "请填写装修详细要求", trigger: "blur" }
+        ],
+        compayRequest: [
+          { required: true, message: "请填写投标公司要求", trigger: "blur" }
+        ]
       }
     };
   },
   methods: {
-    onSubmit() {
-      console.log('submit!');
+    // 提交数据
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          console.log("submit!");
+          this.axios.post("http://172.16.6.60:8080/releaseTender", {
+            linkMan: this.ruleForm.linkMan,
+            phone: this.ruleForm.phone,
+            calusename: this.ruleForm.calusename,
+            area: this.ruleForm.area,
+            location: this.ruleForm.location,
+            timeForProject: this.ruleForm.timeForProject,
+            startTime: this.ruleForm.startTime,
+            familyStructure: this.ruleForm.familyStructure,
+            tenderStartTime: this.ruleForm.tenderStartTime,
+            housingSituation:this.ruleForm.housingSituation,
+            tenderEndTime: this.ruleForm.tenderEndTime,
+            decorationBudget: this.ruleForm.decorationBudget,
+            fitmentRequest: this.ruleForm.fitmentRequest,
+            compayRequest: this.ruleForm.compayRequest
+          })
+          // this.axios({
+          //   url: "http://172.16.6.60:8080/releaseTender",
+          //   method: "post",
+          //   data: `tender=${this.ruleForm}`,
+          //   header: { "content-type": "application/x-www-form-urlencoded"}
+          // })
+          
+          .then(res => {
+            console.log(res);
+            if(res.data.code == 200) {
+              this.$message({
+                showClose: true,
+                message: '发布成功！',
+                type: 'success'
+              });
+            }
+          })
+          .catch(err => {
+            console.log(err);
+          });
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    // 重置输入框
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
     }
   }
-}
+};
 </script>
+
+<style lang="less" scoped>
+</style>
--- src/views/ResetPassword.vue
+++ src/views/ResetPassword.vue
@@ -7,11 +7,11 @@
       </el-form-item>
 
       <el-form-item label="密码" prop="pass">
-        <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+        <el-input type="password" v-model="ruleForm.pass" autocomplete="off" show-password></el-input>
       </el-form-item>
 
       <el-form-item label="确认密码" prop="checkPass">
-        <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+        <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off" show-password></el-input>
       </el-form-item>
   
       <el-form-item>
@@ -92,3 +92,5 @@ export default {
     text-align: center;
   }
 </style>
+
+
