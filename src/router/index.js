2ba60702ad75785d4b6194a6841113dc43ff300f
lin <961234478@qq.com>
2019-12-03 19:03:45 +0800

    L

--- src/router/index.js
+++ src/router/index.js
@@ -67,7 +67,13 @@ const routes = [
     name: 'decorationDesign',
     component: () => import('../views/DecorationDesign.vue')
   },
+  {
+    path: '/design',
+    name: 'design',
+    component: () => import('../views/Design.vue')
+  },
  
+  
   {
     path: '/invitation',
     name: 'invitation',
